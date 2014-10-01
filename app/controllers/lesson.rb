b12573d44b0e35c3bcde0e7222df1e2470f8d32d
Katherine <kdmcclin@gmail.com>
2014-10-01 15:37:16 -0400

    Change when users can edit lessons

--- app/controllers/lesson.rb
+++ app/controllers/lesson.rb
@@ -25,22 +25,22 @@ put '/lessons/:id' do
   #AJAX lesson editing later
 end
 
-get '/lessons/:id/edit_confirmation' do
-  if params[:error] == 'error'
-    @error = "ERROR: USERNAME AND PASSWORD FOR POST DO NOT MATCH"
-  end
-  @lesson = Lesson.find(params[:id])
-  erb :'/lessons/edit_confirmation'
-end
+# get '/lessons/:id/edit_confirmation' do
+#   if params[:error] == 'error'
+#     @error = "ERROR: USERNAME AND PASSWORD FOR LESSON DO NOT MATCH"
+#   end
+#   @lesson = Lesson.find(params[:id])
+#   erb :'/lessons/edit_confirmation'
+# end
 
-post '/lessons/:id/edit_confirmation' do
-  @lesson = Lesson.find(params[:id])
-  if params[:username] == @lesson.user.username && params[:password] == @lesson.user.password
-    redirect ("lessons/#{params[:id]}/edit")
-  else
-    redirect ("/lessons/#{params[:id]}/edit_confirmation?error=error")
-  end
-end
+# post '/lessons/:id/edit_confirmation' do
+#   @lesson = Lesson.find(params[:id])
+#   if params[:username] == @lesson.user.username && params[:email] == @lesson.user.email
+#     redirect ("lessons/#{params[:id]}/edit")
+#   else
+#     redirect ("/lessons/#{params[:id]}/edit_confirmation?error=error")
+#   end
+# end
 
 delete '/articles/:id' do
   @lesson = Lesson.find(params[:id])
